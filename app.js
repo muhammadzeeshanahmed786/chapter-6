a3e6cd2c9bc08619a7278346e37f1fcf000eebaf
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-22 10:58:47 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+
+// var name1=document.getElementById("name");
+// function fun(){
+//     var pro=prompt("enter name")
+//     name1.innerHTML="Well Come "+pro
+// }
+
+// var idChange=document.getElementById('change')
+// let fun=()=>{
+//     idChange.innerHTML="done"
+// }
+
+// var h1=document.getElementById('h1')
+// function fun(){
+//     h1.innerHTML="Ahmed"
+// }
+// function fun1(){
+//     h1.innerHTML="Zeeshan"
+// }
+
+
+
+
+
+
+
+
+
+
+// var h1=document.getElementById('h1')
+// function fun(){
+//     // h1.innerHTML="Ahmed"
+//     h1.style.color ="red"
+// }
+// function fun1(){
+//     h1.innerHTML="Zeeshan"
+
+//     h1.style.color ="black"
+
+// }
+
+
+
+
+
+
+
+
+
+
+// var city=document.getElementById('city')
+// var pro=document.getElementById('pro')
+
+function blur(e){
+    console.log(e.value)
+
+    // if(city.value == "karachi"){
+    //     pro.value="Sindh"
+    // }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
